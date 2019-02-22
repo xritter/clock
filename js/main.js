5970373fd375c451acfdadbb10615d73e7c292c1
ritter signs <ritter.signs@gmx.de>
2019-02-22 11:56:24 +0100

    update the hour- and minhands

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-
 var size = 200;
-var arrColor	=	new Array (
-	'tomato',
+var arrColor = new Array(
+  'tomato',
   'yellow',
   'lime',
   'orange',
@@ -17,41 +16,44 @@ var arrColor	=	new Array (
 );
 
 function init() {
-    showTime();
+  showTime();
 }
 
-function showTime(){
+function showTime() {
   var now = new Date();
   var hour = now.getHours();
   var min = now.getMinutes();
   var sec = now.getSeconds();
-  var color1 = arrColor[rand(0,12)];
-  var color2 = arrColor[rand(0,12)];
-  var color3 = checkColor(color1,color2);
+  var color1 = arrColor[rand(0, 12)];
+  var color2 = arrColor[rand(0, 12)];
+  var color3 = checkColor(color1, color2);
+  var hourDeg = 30 * ((hour % 12));
+  var minDeg = 6 * min;
 
   hour = insert0(hour);
   min = insert0(min);
   sec = insert0(sec);
-  size >= 600 ? size=200 : size;
+  size >= 600 ? size = 200 : size;
 
   document.getElementById('clock').innerHTML =
-  '<h1>'
-  +hour+':'
-  +min+':'
-  +sec;
+    '<h1>' +
+    hour + ':' +
+    min + ':' +
+    sec;
 
   document.getElementById('clock').style.backgroundColor = color1;
-  document.getElementById('clock').style.width = size+'px';
-  document.getElementById('clock').style.height = size+'px';
-  document.getElementById('clock').style.borderWidth = min+'px';
+  document.getElementById('clock').style.width = size + 'px';
+  document.getElementById('clock').style.height = size + 'px';
+  document.getElementById('clock').style.borderWidth = min + 'px';
+  document.getElementById('hour-pointer').style.webkitTransform = 'rotate(' + hourDeg + 'deg)';
+  document.getElementById('min-pointer').style.webkitTransform = 'rotate(' + minDeg + 'deg)';
   document.getElementsByTagName('body')[0].style.backgroundColor = color3
-  size+= 100;
-    console.log(size);
-  var time = setTimeout(showTime, 1000);
+  size += 100;
+  setTimeout(showTime, 1000);
 }
 
-function checkColor(color1,color2){
-  if(color1 == color2){
+function checkColor(color1, color2) {
+  if (color1 == color2) {
     var color = 'darkviolet';
     return color;
   } else {
@@ -60,10 +62,10 @@ function checkColor(color1,color2){
 }
 
 function rand(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function insert0(nr){
-    nr<10?nr='0'+nr:'';
-    return nr;
+function insert0(nr) {
+  nr < 10 ? nr = '0' + nr : '';
+  return nr;
 }
